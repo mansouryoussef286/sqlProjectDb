204aca9f57603946bee93882b04eaacd1d7682ef
hager <hagermontaser5@gmail.com>
2022-03-07 01:07:20 +0200

    student_data_update

--- Data/dbo.students_Data.sql
+++ Data/dbo.students_Data.sql
@@ -1,4 +1,4 @@
-INSERT INTO [dbo].[students] ([stud_ID], [stud_Fname], [stud_Lname], [dept_ID], [stud_Username], [stud_pw]) VALUES (4, N'g', N'g', 1, N'd', N'd')
 INSERT INTO [dbo].[students] ([stud_ID], [stud_Fname], [stud_Lname], [dept_ID], [stud_Username], [stud_pw]) VALUES (1, N'youssef', N'mansour', 1, N'yousseff', N'youssef123')
 INSERT INTO [dbo].[students] ([stud_ID], [stud_Fname], [stud_Lname], [dept_ID], [stud_Username], [stud_pw]) VALUES (2, N'hager', N'montaser', 1, N'hager', N'hager')
-INSERT INTO [dbo].[students] ([stud_ID], [stud_Fname], [stud_Lname], [dept_ID], [stud_Username], [stud_pw]) VALUES (3, N'd', N'd', 1, N'df', N'f')
+INSERT INTO [dbo].[students] ([stud_ID], [stud_Fname], [stud_Lname], [dept_ID], [stud_Username], [stud_pw]) VALUES (4, N'g', N'g', 1, N'd', N'd')
+INSERT INTO [dbo].[students] ([stud_ID], [stud_Fname], [stud_Lname], [dept_ID], [stud_Username], [stud_pw]) VALUES (3, N'nesma', N'safa', 1, N'nesma', N'nesma123')
