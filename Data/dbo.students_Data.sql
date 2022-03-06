ce088e81b75691b4ed7972e3ed89ef4cf35a55b6
youssef mansour <youssef mansour>
2022-03-07 00:05:18 +0200

    1

--- Data/dbo.students_Data.sql
+++ Data/dbo.students_Data.sql
@@ -1,2 +1,3 @@
+INSERT INTO [dbo].[students] ([stud_ID], [stud_Fname], [stud_Lname], [dept_ID], [stud_Username], [stud_pw]) VALUES (3, N'd', N'd', 1, N'df', N'f')
 INSERT INTO [dbo].[students] ([stud_ID], [stud_Fname], [stud_Lname], [dept_ID], [stud_Username], [stud_pw]) VALUES (1, N'youssef', N'mansour', 1, N'yousseff', N'youssef123')
 INSERT INTO [dbo].[students] ([stud_ID], [stud_Fname], [stud_Lname], [dept_ID], [stud_Username], [stud_pw]) VALUES (2, N'hager', N'montaser', 1, N'hager', N'hager')
