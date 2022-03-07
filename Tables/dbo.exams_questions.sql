6333aede14a12d24e0586d2e2a9601b9891ed963
hager <hagermontaser5@gmail.com>
2022-03-07 15:18:40 +0200

    Schema

--- Tables/dbo.exams_questions.sql
+++ Tables/dbo.exams_questions.sql
@@ -3,10 +3,10 @@ CREATE TABLE [dbo].[exams_questions]
 [exam_ID] [int] NOT NULL,
 [q_ID] [int] NOT NULL,
 [answer] [int] NULL,
-[St_ID] [int] NULL
+[St_ID] [int] NOT NULL
 ) ON [PRIMARY]
 GO
-ALTER TABLE [dbo].[exams_questions] ADD CONSTRAINT [PK_exams_questions] PRIMARY KEY CLUSTERED ([exam_ID], [q_ID]) ON [PRIMARY]
+ALTER TABLE [dbo].[exams_questions] ADD CONSTRAINT [PK_exams_questions] PRIMARY KEY CLUSTERED ([exam_ID], [q_ID], [St_ID]) ON [PRIMARY]
 GO
 ALTER TABLE [dbo].[exams_questions] ADD CONSTRAINT [FK_exams_questions_exams] FOREIGN KEY ([exam_ID]) REFERENCES [dbo].[exams] ([exam_ID])
 GO
