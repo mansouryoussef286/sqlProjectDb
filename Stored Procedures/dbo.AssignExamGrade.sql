3b7935b2e1d83f70fdf4e963d393246cca8e2e43
hager <hagermontaser5@gmail.com>
2022-03-07 12:10:41 +0200

    drop assign procedures

--- Stored Procedures/dbo.AssignExamGrade.sql
+++
@@ -1,8 +0,0 @@
-SET QUOTED_IDENTIFIER ON
-GO
-SET ANSI_NULLS ON
-GO
-create proc [dbo].[AssignExamGrade] @examid int,@grade int
-as
-update exams set exam_grade = @grade where exam_ID = @examid
-GO
--- Stored Procedures/dbo.AssignExamStudent.sql
+++
@@ -1,9 +0,0 @@
-SET QUOTED_IDENTIFIER ON
-GO
-SET ANSI_NULLS ON
-GO
-
-create proc [dbo].[AssignExamStudent] @examid int,@stdID int
-as
-update exams set stud_ID = @stdID where exam_ID = @examid
-GO
