d5029fcf767fcb37b48e9e137959dc0e44915aad
Youssef Mansour <mansouryoussef286@gmail.com>
2022-03-08 14:27:13 +0200

    adding fourth report to the schema

--- Stored Procedures/reports.finalResults.sql
+++ Stored Procedures/reports.finalResults.sql
@@ -2,7 +2,7 @@ SET QUOTED_IDENTIFIER ON
 GO
 SET ANSI_NULLS ON
 GO
-create proc [dbo].[finalResults](@std_id int)
+create proc [reports].[finalResults](@std_id int)
 as
 select xs.Exam_ID , st_grade , c_name  from Exam_Student as xs  , exams as x ,courses as c
 where c.c_ID=x.course_ID and x.exam_ID=xs.Exam_ID and xs.St_ID=@std_id
